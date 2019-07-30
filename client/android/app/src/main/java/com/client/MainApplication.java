f86366030587a30d501db4de6ccc0bd01acbe1b6
rubin2 <rubin@softsquared.com>
2019-07-30 22:13:03 +0900

    지도 polyline 띄우는 기능 추가 및 gps권한 허용 부분 추가

--- client/android/app/src/main/java/com/client/MainApplication.java
+++ client/android/app/src/main/java/com/client/MainApplication.java
@@ -8,6 +8,7 @@ import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
 import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.geolocation.GeolocationPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
               new MainReactPackage(),
+            new GeolocationPackage(),
               new MapsPackage()
       );
     }
